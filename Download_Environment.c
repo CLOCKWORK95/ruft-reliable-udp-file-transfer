b84c70bccbec2cd394dfdfa1aa542c769532c327
CLOCKWORK95 <gimbors95@gmail.com>
2019-07-22 19:13:08 +0200

    RUFT

---
+++ Download_Environment.c
@@ -0,0 +1,399 @@
+// Server side implementation of UDP client-server model : DOWNLOAD ENVIRONMENT
+
+#define _POSIX_SOURCE
+#include <stdio.h> 
+#include <pthread.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/signal.h>
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include "support.c"
+#include "Reliable_Data_Transfer.c"
+
+#define _POSIX_SOURCE
+
+#define MAX_WORKERS     5
+
+
+struct block;
+
+
+/*  
+    This structure represents an istance of working thread related to a specific block of the RUFT Server's Download Environment. 
+    Each of the block's workers serves download requests through the same block's socket, but for a different client. 
+    As a worker thread serves a request, it goes on pause, waiting for a signal (SIGUSR1) to be awaked. 
+*/
+typedef struct worker_{
+
+    int                     identifier;                                 //Unique identifier of the worker of a block, to receive ACKs.
+
+    block                  *my_block;                                   //The block containing this worker instance.
+
+    pthread_t               tid;                                        //Identifier of the working-thread.
+
+    struct sockaddr_in      client_addr;                                //Address of the client who made the request.
+
+    int                     sockfd;                                     //Socket descriptor through which sending packets.
+
+    char                    is_working;                                 // '0' : sleeping   |   '1' : working.
+
+    sw_slot                 *sliding_window_slot_;                      //Circular linked list of sliding window's slots, related to this worker instance.
+
+    struct worker_          *next;                                      //Pointer to the next worker of the same block (NULL if this is the last worker).
+
+}               worker;
+
+
+
+
+/*  
+    This structure represents the first sub-division of the RUFT Server's Download Environment.
+    The Download Environment itself is an array of block structs. Each block is related to a specific file on Server's persistance. 
+    As a new block is allocated, a new socket is created, together with a pool of threads working exclusively for that block (workers).
+    Each block refers to a file stream (buffer cache) to make workers trasmit packets fast.
+    Each block has a life timer countdown, that starts running as all workers are on pause. if timer runs out, the block instance is deallocated.
+*/
+typedef struct block_ {
+
+    char                    *filename;                                  //Name of the file associated to this block.
+
+    FILE                    *buffer_cache;                              //Buffer cache containing the file to transmit.
+
+    int                     server_sock_desc;                           //Block's (new) socket descriptor.
+
+    worker                  *workers;                                   //Array of block's workers. 
+
+    pthread_t               ack_dmplx_thread;                            //Acknowledgments demultiplexer thread's TID.
+
+    struct block_           *next;                                      //Pointer to next block structure.
+
+    int                     BLTC;                                       //Block Life Timer Countdown.
+
+
+}               block;                                                  extern      block        *download_environment;
+
+
+        /*  THREADS' FUNCTIONS DECLARATION */
+
+void * work( void* _worker );
+
+
+void * acknowledgment_demultiplexer( void * _block );
+
+
+
+
+/*  
+    This function returns a new Download Environment's block at the address specified in *new_block.
+    In addition to many attributes, the generated block contains a stream of file specified in pathname, a pool of threads 
+    and a new operating socket. This function activates a first thread, serving the request the function has been called to.  
+    Returns : 0 on success, -1 on Error.
+*/
+int init_new_block ( block *new_block, char * pathname , struct sockaddr_in client_address ){ 
+
+    int ret;    
+
+    //validate memory area to contain a new block structure.
+    new_block = malloc( sizeof( block ) );
+    if (new_block == NULL) {
+        printf("Error in function : malloc");
+        return -1;
+    }
+
+    /*  Populate the block structure attributes. */
+    
+    ret = sprintf( ( new_block -> filename ), pathname );                                   //match a file path.
+    if (ret == -1) {
+        printf("Error in function : sprintf.", 1);
+    }
+
+    new_block -> buffer_cache = fopen( pathname, "r");                                      //open a (readonly) session on file and create the file stream.
+    if (new_block -> buffer_cache == NULL) {
+        printf("Error in function : fopen.");
+        return -1;
+    }
+
+    new_block -> BLTC = (int) ( strlen( new_block -> buffer_cache ) / PACKET_SIZE  ) ;      //set the BLTC default value proportional to the file size.
+
+    
+    if ( ( ( new_block -> server_sock_desc ) = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {      //creating block's socket file descriptor 
+        perror("socket creation failed (init new block)."); 
+        return -1; 
+    }
+
+
+    worker *tmp = new_block -> workers;                                                     
+
+    for ( int i = 0; i < MAX_WORKERS; i ++) {                                               //validate memory area for all workers.
+
+        tmp = malloc( sizeof( worker ) );
+        if (tmp == NULL) {
+            printf("Error in function : malloc ( init new block).");
+            return -1;
+        }
+
+        //populate worker's attributes.
+
+        tmp -> identifier = i;
+
+        tmp -> is_working = '0';
+
+        tmp -> sockfd = new_block -> server_sock_desc;
+
+        tmp -> my_block = new_block;
+
+        if ( i != MAX_WORKERS -1)   tmp = ( tmp -> next );
+        else                        tmp -> next = ( new_block -> workers );                 //this makes a circular linked list of workers.
+        
+    }
+
+
+    /*  Generate the thread-pool and activate the new thread to serve the request.  */
+
+    tmp = ( new_block -> workers );
+
+    tmp -> is_working = '1';
+
+    tmp -> client_addr = client_address;
+
+
+    /*  Create the Acknowledgment Multiplexer Thread, to handle acknowledgments throughout the block. */
+
+    ret = pthread_create( ( new_block -> ack_dmplx_thread ), NULL, acknowledgment_demultiplexer, (void *) new_block );
+    if (ret ==-1) {
+        printf("Error in function : pthread_create(init_new_block).");
+        return -1;
+    }
+
+
+    /*  Create and launch all concurrent threads of the pool. */
+
+    for ( int i = 0; i < MAX_WORKERS; i ++ ) {
+
+        ret = pthread_create( &( tmp -> tid ), NULL, work, (void *) tmp );
+        if ( ret == -1 ) {
+            printf("Error in function: pthread_create (init_new_block).");
+            return -1;
+        }
+
+        tmp = ( tmp -> next );
+    }
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+/*  
+    This function is tipically called by the Reception Environment of RUFT Server.
+    By this function, server reception delegates download environment sending to client_address the file specified in pathname.
+    Download Environment's blocks are checked to find the one matched to that file, if already exists: 
+    in that case, a paused thread of the block is awaken by SIGUSR1 event ( function pthread_kill ).
+    If a block matched to pathname doesn't exists yet in Download Environment, this function calls "init_new_block" to allocate a new one.
+    Returns : 0 on success, -1 on error.
+*/
+int start_download( char *pathname , struct sockaddr_in client_address ) {
+
+    int ret;
+
+    block *tmp_block = download_environment;
+
+    block *last_block = NULL;
+
+    do {
+
+        if ( strcmp( pathname, ( tmp_block -> filename ) ) == 0 ) {
+
+
+            /* if the requested file to download is already present in one of the existing blocks
+               of download environment, then the first paused worker of that block is chosen to
+               serve this request. */
+
+            worker *tmp_worker = tmp_block -> workers;
+
+            do {
+
+                if ( tmp_worker -> is_working == '0' ){
+
+                    //awake the sleeping thread and request the service.
+
+                    tmp_worker -> client_addr = client_address;
+
+                    ret = pthread_kill( ( tmp_worker -> tid ) , SIGUSR1 );
+                    if (ret != 0) {
+                        printf("Error in function: pthread_kill (start_download).");
+                        return -1;
+                    }
+
+                    return 0;
+
+                }
+
+            } while ( ( tmp_worker = ( tmp_worker -> next ) ) != NULL);
+
+            printf("\nOps! all block's threads are already in use. Not able to serve a request in an existing block... ");
+
+        }
+
+        last_block = tmp_block;
+
+
+    } while ( ( tmp_block = (tmp_block -> next ) ) != NULL );
+
+
+    /*  if the following block of code is going to be executed, it means there are not available blocks 
+        (or free workers) to serve the download request on this file. 
+        A new block related to the requested file is going to be allocated, so that this request can be
+        handled. */
+
+    printf("\n\n Allocating a new block for file %s in Server's Download Environment...");
+
+    ret = init_new_block( last_block -> next, pathname, client_address);
+    if (ret == -1) {
+        printf("Error in function: init_new_block (start_download).");
+        return -1;
+    }
+
+    return 0;
+
+}
+
+
+
+
+        /*  EVENT HANDLERS DECLARATION : SIGUSR1 & SIGUSR2 */
+
+void wake_up();
+
+void incoming_ack();
+
+
+
+
+
+        /*  THREADS' FUNCTIONS  */
+
+/*  
+    This is block's worker-threads' function. 
+    This function splits each thread's life into two main parts, one of pause (sleeping), the other concerning transmissions of requested files. 
+    As a thread is awaken, it starts forwarding packets till the whole file is transferred.
+    Once a thread ends its job, it alerts the parent-block  and increment block's accesses value, then go back to pause.
+*/
+void * work ( void * _worker ) {
+
+    signal( SIGUSR1, wake_up);              //Set the waking up event handler.
+    signal( SIGUSR2, incoming_ack);         //Set the ACKNOWLEDGMENT event handler (signal has to be forwarded by a specialized thread, working for all block's workers).
+
+    int ret;
+
+    worker  * me = ( worker * ) _worker;
+
+    block   * myblock = ( me -> my_block );
+
+    redo:
+
+    myblock -> BLTC ++;
+
+    ret = reliable_file_transfer( (me -> identifier), ( me -> sockfd ), ( me -> client_addr ), ( me -> my_block-> buffer_cache ), ( me -> sliding_window_slot_ ) );
+    if (ret == -1) {
+        printf("Error in function : reliable_data_trasnfer.");
+        goto redo;
+    }
+
+
+    me -> is_working = '0';
+
+    pause();
+
+    goto redo;
+
+
+}
+
+
+
+/*  
+    This is the block's acknowledgment keeper thread function.
+    By this function, this thread receives block's client's acknowledgments and executes demultiplexing of them: 
+    each ACK is directed to a specific block's worker, and further to a specific slot of its sliding window.
+    This thread is responsible for notifying workers about the received ACKs and for awakening a worker waiting sliding his window.
+*/
+void * acknowledgment_demultiplexer( void * _block ){
+
+
+    int     ret,    len;
+
+    char    *id,            *seq_num;
+
+
+    char    *buffer = malloc( sizeof( char ) * ACK_SIZE );
+
+    block   *myblock = ( block * ) _block;
+
+    worker  *w_tmp = ( myblock -> workers );
+
+    sw_slot *sw_tmp;
+
+
+    do {
+
+        /*  Receive a packet (acknowledgments) from related block's socket.  */
+
+        struct sockaddr     client_address;
+
+        memset( buffer, 0, sizeof( buffer ) );
+
+        ret = recvfrom( myblock -> server_sock_desc, (char *) buffer, ACK_SIZE , MSG_WAITALL, ( struct sockaddr *) &client_address, &len); 
+        if (ret <= 0)       Error_("Error in function : recvfrom (acknowledgment_demultiplexer).", 1);
+
+        /*  Parse the packet to keep separated the identifier and sequence number fields.  */
+
+        ret = sprintf( id, "%s", strtok( buffer, "/" ) );
+        if (ret == -1)      Error_("Error in function sprintf (acknowledgment_demultiplexer).", 1);
+
+        ret = sprintf( seq_num, "%s", strtok( NULL, "/" ) );
+        if (ret == -1)      Error_("Error in function sprintf (acknowledgment_demultiplexer).", 1);
+
+
+        /*  Find the block's worker with identifier as specified on ACK. */
+
+        while ( ( w_tmp -> identifier ) != atoi(id) ) {
+
+            w_tmp = ( w_tmp -> next );
+
+        }
+
+        /*  Once the worker is found, find the worker's window's slot with sequence number as specified on ACK  */
+
+        sw_tmp = ( w_tmp -> sliding_window_slot_ );
+
+        while ( ( sw_tmp -> sequence_number ) != atoi(seq_num) ) {
+
+            sw_tmp = ( sw_tmp -> next );
+
+        }
+
+
+        /*  Update worker window's slot's status from SENT to ACKED. 
+            If the slot is the first of the sliding window, forward a SIGUSR2 signal to worker-thread to get the window sliding. */
+
+        if ( ( sw_tmp -> status ) != SENT )     Error_("Error in acknowledgment handling : unexpected window's status.", 1);
+
+        sw_tmp -> status = ACKED;
+
+        if ( ( sw_tmp -> is_first ) == '1' )    pthread_kill( ( w_tmp -> tid ), SIGUSR2 );          
+
+
+    } while (1);
+
+}
---
+++ Reliable_Data_Transfer.c
@@ -0,0 +1,274 @@
+/*  Here is implemented all of the Reliable Transfer Logic, used by transferring sections of RUFT client's and server's software. */
+
+#define _POSIX_SOURCE
+#include <signal.h>
+#include <stdio.h> 
+#include <pthread.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+/* RDT CONFIGURABLE PARAMETERS */
+
+#define TIMEOUT_INTERVAL    1
+
+#define LOSS_PROBABILITY    6
+
+#define WINDOW_SIZE         7
+
+#define PACKET_SIZE         512
+
+#define HEADER_SIZE         25
+
+#define ACK_SIZE            30
+
+
+/* DEFINE SLIDING WINDOW STATES */
+
+#define FREE                0
+
+#define SENT                1
+
+#define ACKED               2
+
+
+/* DEFINE RECEIVING WINDOW STATES */ 
+
+#define WAITING             3
+
+#define RECEIVED            4
+
+
+
+
+
+typedef struct sliding_window_slot_ {
+
+    int                     sequence_number;                            //Sequence number of related packet.
+
+    int                     bytes;                                      //Actual number of packet's bytes
+
+    char                    is_first;                                   // '0' : not first    |    '1' : first.    
+
+    int                     status;                                     // FREE - SENT - ACKED
+
+    struct sliding_window_slot_     *next;                              //Pointer to the next slot.
+
+}               sw_slot;
+
+
+
+
+
+
+typedef struct receiving_window_slot_ {
+
+    int                     sequence_number;                            //Sequence number of related packet.                             
+
+    int                     status;                                     // WAITING - RECEIVED
+
+    struct receiving_window_slot_     *next;                            //Pointer to the next slot.
+
+}               rw_slot;
+
+
+
+
+
+
+/*  This function allocates, initiate and returns a new sliding window of size WINDOW_SIZE, as defined at the top of this file.   */
+sw_slot*    get_sliding_window(){
+
+    sw_slot * window;
+
+    window = malloc( sizeof( sw_slot ) );
+    if (window == NULL){
+        printf("Error in function : malloc (get_sliding_window).");
+        return NULL;
+    }
+
+    window -> is_first = '1';
+    window -> sequence_number = 0;
+    window -> status = FREE;
+
+    sw_slot * tmp = window;
+
+    for ( int i = 1 ; i < WINDOW_SIZE ; i ++ ) {
+
+        tmp -> next = malloc( sizeof( sw_slot ) );
+        if ( ( tmp -> next ) == NULL) {
+            printf("Error in function : malloc (get_sliding_window).");
+            return NULL;
+        }
+
+        tmp = ( tmp -> next );
+
+        tmp -> is_first = '0';
+        tmp -> sequence_number = i;
+        tmp -> bytes = 0;
+        tmp -> status = FREE;
+
+    }
+
+    tmp -> next  = window;
+
+    return window;
+
+}
+
+
+
+
+
+
+/*  
+    This function implements the reliable transfer logic. 
+    This function takes as argument a File Stream containing the file to forward, specified in buffer_cache.
+    The file has to be sent to the client_address through the block's socket specified in socket_descriptor.
+    Reliable Data Transfer is implemented as a pipelining ' sliding window 'protocol, using ACKs and timout-retransmissions.
+*/
+int reliable_file_transfer ( int identifier, int    socket_descriptor, struct sockaddr_in    client_address , FILE  *buffer_cache , sw_slot   *window) {
+
+    int         ret,    len,    filesize,   counter = 0;
+
+    /*  Get the sliding window for this transfer occurrence. */
+
+    window = get_sliding_window();                                 //returns a pointer to a linked circular list of sliding window's slots.
+
+    sw_slot     *tmp =                  window;
+
+    sw_slot     *first_free =           window;
+
+
+    char        *packet = malloc( HEADER_SIZE + PACKET_SIZE );                            
+    if (packet == NULL) {
+        printf("Error in function : malloc (reliable data transfer).");
+        return -1;
+    }
+
+    char        *packet_header = malloc ( HEADER_SIZE );
+    if (packet_header == NULL) {
+        printf("Error in function : malloc (reliable data transfer).");
+        return -1;
+    }
+
+    filesize =  strlen( buffer_cache );                                         //variable used to check if all file has been sent after transaction.
+
+    
+    /* Temporarily block all signals to this thread. */
+
+    sigset_t set;
+    sigfillset( &set );
+    sigprocmask( SIG_BLOCK, &set, NULL);
+    
+
+    do {
+
+
+        while ( ( tmp -> status ) == FREE ) {
+
+            /*  Populate packet's contents ( worker identifier, sequence number and related chunck of file bytes). */
+
+            memset( packet, 0, sizeof( packet ) );
+
+            ret = sprintf( packet, "%d/", identifier );                          
+            if (ret == -1) {
+                printf("Error in function: sprintf (reliable_file_transfer).");
+                return -1;
+            }
+
+            ret = sprintf( ( packet + strlen( packet ) ), "%d/", ( tmp -> sequence_number ) );
+            if (ret == -1) {
+                printf("Error in function: sprintf (reliable_file_transfer).");
+                return -1;
+            }
+
+            ret = sprintf( packet_header, "%s", packet );                                            //write the actual header on header packet string.
+            if (ret == -1) {
+                printf("Error in function : sprintf (reliable data transfer).");
+                return -1;
+            }
+
+            ret = snprintf( ( packet + strlen( packet ) ), PACKET_SIZE, "%s", buffer_cache + ( PACKET_SIZE * ( tmp -> sequence_number ) ) );
+            if (ret == -1) {
+                printf("Error in function : snprintf (reliable data transfer).");
+                return -1;
+            }
+
+
+            /*  Send the packet to the client through the block's socket */
+
+            ret = sendto( socket_descriptor, (const char *) packet, strlen(packet), MSG_CONFIRM, (const struct sockaddr *) &client_address, sizeof( client_address ) ); 
+            if (ret == -1) {
+                printf("Error in function sendto (reliable_file_transfer).");
+                return -1;
+            }
+
+
+            tmp -> bytes += strlen( packet + strlen( packet_header ) );                               //set slot's bytes value with the precise number of file bytes sent.
+            
+            tmp -> status = SENT;                                                                     //update sliding window's state.
+
+            tmp = ( tmp-> next );
+
+        }
+
+        
+        /*  Here thread is going on pause until the first window's slot's state is set to ACKED.
+            Pause state expects a signal to let the thread being awaken. 
+            Signal expected is SIGUSR2, forwarded by the ACK-Thread of the block and used to 
+            notify an acknowledgment to a worker.   */
+        
+        sigemptyset( &set );
+        sigaddset( &set, SIGUSR2);
+        sigprocmask( SIG_UNBLOCK, &set, NULL);
+        
+        pause();
+
+        /*  Temporarily block all signals to this thread.  */
+
+        sigset_t set;
+        sigfillset( &set );
+        sigprocmask( SIG_BLOCK, &set, NULL);
+
+
+        first_free = tmp;
+
+        while ( ( tmp -> status ) == ACKED ) {
+
+            tmp -> status = FREE;                                                //reset slot's status to FREE, to be reused by next packets.                                         
+
+            counter += ( tmp -> bytes );                                         //update counter value with acknowledged bytes of this slot.
+
+            tmp -> bytes = 0;                                                    //reset slot's bytes to 0, to be reused by next packets.
+
+            tmp -> is_first = '0';                                               //set the is_first flag to '0' : this slot is becoming the last one of the window.
+
+
+            /*  Slide the window on. */
+
+            tmp -> sequence_number += WINDOW_SIZE;
+            tmp = ( tmp -> next );
+            tmp -> is_first = '1';
+            window = tmp;    
+                                                              
+
+        }
+
+        /*  Set tmp value to the first FREE slot to start over the cycle from the top.  */
+
+        tmp = first_free;
+
+ 
+
+    } while( counter < filesize );
+
+
+    return counter;
+
+
+}
