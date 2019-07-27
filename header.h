10da9b52f7a04ecbb6ff77c93de29886079ebedd
CLOCKWORK95 <gimbors95@gmail.com>
2019-07-27 23:35:59 +0200

    RUFT

---
+++ header.h
@@ -0,0 +1,24 @@
+#pragma once
+#define _POSIX_C_SOURCE 199309L
+#include <signal.h>
+#include <stdio.h> 
+#include <pthread.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <dirent.h>
+#include <time.h>
+
+#define Error_(x,y)     { puts(x); exit(y); }
+#define MAXLINE 1024 
+
+extern long                TIMEOUT_INTERVAL;
+extern struct timespec     beat;
+
+
