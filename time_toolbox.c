54b89d806cfb8269fbef849aabdf133b617007d8
Cristina Ionne <cristina@linux-ncb8.suse>
2019-07-28 17:51:04 +0200

    time_toolbox.c edited

--- time_toolbox.c
+++ time_toolbox.c
@@ -34,3 +34,26 @@ long nanodifftime ( struct timespec *time1, struct timespec *time2 ){
     return nanodiff;
 
 }
+
+/*calculates the adaptive RTT according to RFC 6298's rules. */
+long get_adaptive_TO(struct timespec *in_time, struct timespec *fin_time){
+	
+	long estimatedRTT = TIMEOUT_INTERVAL;
+	
+	long devRTT,		toInterval;
+	
+	long timeIn = ( in_time -> tv_nsec ) + ( (1.0e9) * ( in_time -> tv_sec ) );  
+
+	long timeFin = ( fin_time -> tv_nsec ) + ( (1.0e9) * ( fin_time -> tv_sec ) );  
+
+	long sampleRTT = (timeFin - timeIn);
+
+	estimatedRTT = (0.875 * estimatedRTT) + (0.125 * sampleRTT);
+	devRTT = (0.75 * devRTT) + (0.25 * (fabs(sampleRTT - estimatedRTT)));
+	toInterval = estimatedRTT + (4 * devRTT);
+	
+	return toInterval;	
+	
+	} 
+	
+		
